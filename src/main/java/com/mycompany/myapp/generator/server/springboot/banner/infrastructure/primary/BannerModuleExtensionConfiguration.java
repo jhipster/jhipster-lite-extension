9294d44aa1e8392696beb6c7ce24c088aeec1674
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-06-13 12:53:34 -0300

    refactor(package): move application and infrastructure packages outside from domain package

--- src/main/java/com/mycompany/myapp/generator/server/springboot/banner/infrastructure/primary/BannerModuleExtensionConfiguration.java
+++ src/main/java/com/mycompany/myapp/generator/server/springboot/banner/infrastructure/primary/BannerModuleExtensionConfiguration.java
@@ -1,9 +1,9 @@
-package com.mycompany.myapp.generator.server.springboot.banner.domain.infrastructure.primary;
+package com.mycompany.myapp.generator.server.springboot.banner.infrastructure.primary;
 
 import static com.mycompany.myapp.shared.slug.domain.JHLiteSampleExtensionFeatureSlug.*;
 import static com.mycompany.myapp.shared.slug.domain.JHLiteSampleExtensionModuleSlug.*;
 
-import com.mycompany.myapp.generator.server.springboot.banner.domain.application.BannerApplicationExtensionService;
+import com.mycompany.myapp.generator.server.springboot.banner.application.BannerApplicationExtensionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleOrganization;
