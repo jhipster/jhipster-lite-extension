99c8f49f1a817c7b569c52fa09d845b4eac9bd32
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-06-17 10:12:51 -0300

    refactor(slug): remove the extension suffix

--- src/main/java/tech/jhipster/lite/extension/shared/slug/domain/JhliteExtensionSampleModuleSlug.java
+++ src/main/java/tech/jhipster/lite/extension/shared/slug/domain/JhliteExtensionSampleModuleSlug.java
@@ -7,11 +7,11 @@ import java.util.stream.Stream;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleSlugFactory;
 
 public enum JhliteExtensionSampleModuleSlug implements JHipsterModuleSlugFactory {
-  BANNER_JHIPSTER_V2("banner-jhipster-v2-extension"),
-  BANNER_JHIPSTER_V3("banner-jhipster-v3-extension"),
-  BANNER_JHIPSTER_V7("banner-jhipster-v7-extension"),
-  BANNER_JHIPSTER_V7_REACT("banner-jhipster-v7-react-extension"),
-  BANNER_JHIPSTER_V7_VUE("banner-jhipster-v7-vue-extension");
+  BANNER_JHIPSTER_V2("banner-jhipster-v2"),
+  BANNER_JHIPSTER_V3("banner-jhipster-v3"),
+  BANNER_JHIPSTER_V7("banner-jhipster-v7"),
+  BANNER_JHIPSTER_V7_REACT("banner-jhipster-v7-react"),
+  BANNER_JHIPSTER_V7_VUE("banner-jhipster-v7-vue");
 
   private static final Map<String, JhliteExtensionSampleModuleSlug> moduleSlugMap = Stream.of(values()).collect(
     Collectors.toMap(JhliteExtensionSampleModuleSlug::get, Function.identity())
