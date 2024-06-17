3acbd7021839f58d9e1d294207f4a4682d1f4318
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-06-17 10:17:58 -0300

    refactor(JhliteExtensionSampleModuleSlug): remove unused map declaration

--- src/main/java/tech/jhipster/lite/extension/shared/slug/domain/JhliteExtensionSampleModuleSlug.java
+++ src/main/java/tech/jhipster/lite/extension/shared/slug/domain/JhliteExtensionSampleModuleSlug.java
@@ -1,9 +1,5 @@
 package tech.jhipster.lite.extension.shared.slug.domain;
 
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleSlugFactory;
 
 public enum JhliteExtensionSampleModuleSlug implements JHipsterModuleSlugFactory {
@@ -13,9 +9,6 @@ public enum JhliteExtensionSampleModuleSlug implements JHipsterModuleSlugFactory
   BANNER_JHIPSTER_V7_REACT("banner-jhipster-v7-react"),
   BANNER_JHIPSTER_V7_VUE("banner-jhipster-v7-vue");
 
-  private static final Map<String, JhliteExtensionSampleModuleSlug> moduleSlugMap = Stream.of(values()).collect(
-    Collectors.toMap(JhliteExtensionSampleModuleSlug::get, Function.identity())
-  );
   private final String slug;
 
   JhliteExtensionSampleModuleSlug(String slug) {
